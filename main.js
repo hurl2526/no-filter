21c0ecb7a9aa244399372767efdd992efce2b540
Patrick <patrick.hurley@codeimmersives.com>
2020-02-25 14:48:12 -0500

    finished

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+function onlyOdds (numbersArray){
+  let odds = [];
+  for (let i = 0; i < numbersArray.length; i++){
+    if (numbersArray[i] % 2 === 1)
+    {odds.push(numbersArray[i]);}
+  }
+return odds;
+}
+onlyOdds([1,2,3,4]);
 
+function onlyEvens (numbersArray){
+  let even = [];
+  for (let i = 0; i < numbersArray.length; i++){
+    if (numbersArray[i] % 2 === 0)
+    {even.push(numbersArray[i]);}
+  }
+return even;
+}
+onlyEvens([1,2,3,4]);
 
+function shortNamesOnly(namesArray){
+  let short = [];
+  for (let i = 0; i < namesArray.length; i++)
+    if (namesArray[i].length < 7){
+      short.push(namesArray[i]);
+    }
+return short;
+}
+
+function dNames (namesArray){
+  let dNames = [];
+  for (let i = 0; i < namesArray.length; i++){
+    if (namesArray[i][0] === 'D') {
+      dNames.push(namesArray[i]);
+    }
+  }
+  return dNames;
+}
+
+dNames(['Dijon', 'Mustard'])
 
 
 
